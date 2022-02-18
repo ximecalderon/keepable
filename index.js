fe008787762b83688467b7af65d957fa4f6bf5cc
AustinLinares <linaresaustin@hotmail.com>
2022-02-18 09:50:03 -0500

    main view almost done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function DOMHandler(parentSelector) {
   };
 };
 
-let App = DOMHandler(".card-container");
+
 
 const Store = (function () {
   const initialCards = [
@@ -21,9 +21,13 @@ const Store = (function () {
       class: "pink-100-bg",
     },
     {
-      category: "Note 2",
+      title: "Note 2",
       description: "Desc 2",
       class: "blue-200-bg",
+    },
+    { title: "Note 2",
+      description: "Desc 2",
+      class: "pink-100-bg",
     },
   ];
 
@@ -68,7 +72,9 @@ function cardsView() {
   </div>`
   }
 
-  const template = `${Store.cards.map(renderCard).join("")}`;
+  const template = `
+    ${Store.cards.map(renderCard).join("")}
+  `;
 
   return {
     toString() {
@@ -79,8 +85,7 @@ function cardsView() {
   }
 }
 
-const Cards = cardsView();
-App.load(Cards);
+
 
 const Module = (function() {
   const template = ``;
@@ -92,3 +97,73 @@ const Module = (function() {
     addListeners(){}
   }
 })
+
+const formView = (function() {
+  const template = `
+  <form action="" class="#">
+          <div class="input__container padding">
+            <div class="full-width">
+              <input
+                type="text"
+                placeholder="Title"
+                class="input__content heading"
+              />
+              <input
+                type="text"
+                placeholder="Take a note..."
+                class="input__content"
+              />
+            </div>
+            <div class="card__footer full-width">
+              <div class="card__icon--custom">
+                <a href="#" class="to-white"
+                  ><img
+                    src="/assets/icons/palette.svg"
+                    alt="icon-color"
+                    class="center"
+                /></a>
+              </div>
+              <!-- <div> -->
+              <button type="submit" class="button__custom">Keep it!</button>
+              <!-- </div> -->
+            </div>
+          </div>
+        </form>
+  `;
+
+  return {
+    toString() {
+      return template
+    },
+    // addListeners(){}
+  }
+});
+
+
+const Layout = (function() {
+  const template = `
+  ${formView}
+  // <section class="card-container">
+  
+  // </section>
+  `;
+
+  return {
+    toString() {
+      return template
+    },
+    // addListeners(){
+    // }
+  };
+})
+
+
+let App = DOMHandler("#main");
+mainView = Layout();
+App.load(mainView);
+
+let Cards = DOMHandler(".card-container");
+const cartas = cardsView();
+Cards.load(cartas);
+
+
