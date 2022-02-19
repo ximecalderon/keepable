86245a9f9f655512495bc265b71d10d7c6a05707
katerin <ckaterinarellano@gmail.com>
2022-02-18 19:14:18 -0500

    add trash

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ function DOMHandler(parentSelector) {
 
 
 const Store = (function () {
+  const idGenerator = (() => {
+    let id = 0;
+
+    return {
+      next: () => ++id,
+    };
+  })();
+
   const initialCards = [
     {
       title: "Note 1",
@@ -27,19 +35,54 @@ const Store = (function () {
     },
   ];
 
+  /*********************** */
+  const trashsCards = [
+    { 
+      //id: idGenerator.next(),
+      title: "Note 3",
+      description: "Desc 3",
+      class: "pink-100-bg",
+    },
+    {
+      //id: idGenerator.next(),
+      title: "Note 4",
+      description: "Desc 4",
+      class: "blue-200-bg",
+    },
+  ];
+   /*********************** */
+
   return {
     cards: JSON.parse(localStorage.getItem("cards")) || initialCards,
     createCard(card) {
       this.cards.push(card);
       localStorage.setItem("cards", JSON.stringify(this.cards));
     },
+
     deleteCard(card) {
       const index = this.cards.indexOf(card);
       this.cards.splice(index, 1);
       localStorage.setItem("cards", JSON.stringify(this.cards));
     },
+
+  /******************************** */
+    trashs: JSON.parse(localStorage.getItem("trashs")) || trashsCards,
+    createTrash(trash) {
+      this.trashs.push(trash);
+      localStorage.setItem("trashs", JSON.stringify(this.trashs));
+    },
+
+    deleteTrash(trash) {
+      const index = this.cards.indexOf(trash);
+      this.trashs.splice(index, 1);
+      localStorage.setItem("trashs", JSON.stringify(this.cards));
+    },
+
+  /******************************** */
   };
 })();
+
+
 
 
 function cardsView() {
@@ -60,7 +103,7 @@ function cardsView() {
       /></a>
     </div>
     <div class="card__icon--custom">
-      <a href="#" class="to-white"
+      <a href="#" class="to-white" data-id="${expense.id}"
         ><img src="assets/icons/trash_gray.svg" alt="icon-trash"
       /></a>
     </div>
@@ -81,6 +124,58 @@ function cardsView() {
   }
 }
 
+/************************************ */
+const trashView = function (){
+  const renderCard = (card) => {
+    return `<div class="card__content ${card.class} js-trash">
+  <div class="card__text">
+    <p class="heading">${card.title}</p>
+    <p>${card.description}</p>
+  </div>
+
+  <div class="card__icon">
+    <div class="card__icon--custom">
+    <a href="#" class="to-white"
+    ><img src="assets/icons/trash_gray.svg" alt="icon-trash"
+  /></a>
+    </div>
+    <div class="card__icon--custom">
+      <a href="#"><img src="assets/icons/arrow.svg" alt="icon-color" 
+      class="center js-delete"></a>
+    </div>
+  </div>
+  </div>`;
+  };
+  
+
+  const template = `
+    ${Store.trashs.map(renderCard).join("")}
+  `;
+
+  const trashDelete = () => {
+    const trashView = document.querySelector(".js-trash");
+
+    trashView.addEventListener("click", (event) => {
+      event.preventDefault();
+      if (!event.target.classList.contains("js-delete")) return;
+
+      const id = event.target.dataset.id;
+      Store.deleteTrash(id);
+    });
+  };
+
+  return {
+    toString() {
+      return template
+    },
+    addListeners(){
+      trashDelete();
+    },
+  };
+}
+
+
+/******************************************* */
 
 
 const Module = (function() {
@@ -186,4 +281,6 @@ let Cards = DOMHandler(".card-container");
 const cartas = cardsView();
 Cards.load(cartas);
 
-
+//let ContentTrash = DOMHandler(".card-container");
+//const trashVi = trashView();
+//ContentTrash.load(trashVi);
